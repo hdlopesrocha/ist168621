57340c8c925e5fe06eee7792dc1ea68b91d1ab6c
hdlopesrocha <hdlopesrocha91@gmail.com>
2015-03-14 11:44:20 +0000

    [ADD] added page to add IP

--- webrtc/app/controllers/Application.java
+++ webrtc/app/controllers/Application.java
@@ -1,18 +1,27 @@
 package controllers;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 
+		public static List<String> ips = new ArrayList<String>(); 
+	
     public static Result index() {
-        return ok(views.html.index.render());
+        return ok(views.html.index.render(ips));
     }
 
     public static Result sample() {
         return ok(views.html.sample.render());
     }
 
+    public static Result add(String ip){
+    	 ips.add(ip);
+    	 return ok();
+    }
     
 }
--- webrtc/test/ApplicationTest.java
+++ webrtc/test/ApplicationTest.java
@@ -1,11 +1,4 @@
 
-import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.contentType;
-
-import org.junit.Test;
-
-import play.twirl.api.Content;
 
 
 /**
@@ -16,18 +9,6 @@ import play.twirl.api.Content;
 */
 public class ApplicationTest {
 
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
-    }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
-    }
 
 
 }
