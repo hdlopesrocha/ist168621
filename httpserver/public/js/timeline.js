32921acaef76465736f6241d951d9655d4aa65a1
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-08-23 15:24:11 +0100

    [RFT] minor refactor

---
+++ httpserver/public/js/timeline.js
@@ -0,0 +1,42 @@
+
+
+function createTimeline(items,divId, selected) {
+    var main = document.getElementById(divId);
+    var options = {
+        stack: false,
+        align: 'center',
+        showCurrentTime:false
+     // editable: true,
+     // clickToUse: true
+    };
+
+    var timeline =  new vis.Timeline(main, items, options);
+	timeline.addCustomTime(new Date(),"time");
+    
+    timeline.on('rangechange', function(properties){
+    	var start = properties.start;
+    	var end = properties.end;
+    	var avg = new Date((start.getTime()+end.getTime())/2);
+
+    	timeline.removeCustomTime("time");
+    	timeline.addCustomTime(avg,"time");
+    	timeline.setCurrentTime(avg);
+    });
+    
+    timeline.on('select', function (properties) {
+    	var sel = properties.items[0];
+	    if(sel!=undefined){
+	    	selected(sel);
+	    }
+    });    
+
+	followerWorker(timeline);
+    return timeline;
+}
+
+function followerWorker(timeline){
+	var now = timeline.getCurrentTime();
+	timeline.moveTo(now);
+	setTimeout(function(){followerWorker(timeline);},1000);
+}
+
