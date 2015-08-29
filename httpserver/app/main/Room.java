173e3f377d9821fbe9ee69021fd010932d01a24f
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-08-29 03:52:14 +0100

    [FIX] audio for first 10s

--- httpserver/app/main/Room.java
+++ httpserver/app/main/Room.java
@@ -84,6 +84,7 @@ public class Room implements Closeable {
 		RecorderEndpoint rec = new RecorderEndpoint.Builder(mediaPipeline,filepath).build();
 		PlayerEndpoint player = new PlayerEndpoint.Builder(mediaPipeline, filepath).build();
 		Date begin = new Date();
+	
 		
 		rec.addElementDisconnectedListener(new EventListener<ElementDisconnectedEvent>() {
 
