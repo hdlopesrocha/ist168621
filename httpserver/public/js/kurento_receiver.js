7ff0e464d4eaeda7fd1948a0529a14eab5e36132
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-09-01 02:44:11 +0100

    [RFT] single video per user

--- httpserver/public/js/kurento_receiver.js
+++ httpserver/public/js/kurento_receiver.js
@@ -70,7 +70,7 @@ var KurentoReceiver = new (function() {
 		case 'participants':
 			console.log(id,message);
 			for(var userId in message.data){
-				newParticipantsCallback(userId, message.data[userId]);
+				newParticipantsCallback(message.data[userId]);
 				KurentoReceiver.onNewParticipants(userId, message.data[userId]);
 			}
 			break;
