13f314319114a326567e6b63fcb427866893a8da
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-09-09 16:56:41 +0100

    [DEBUG] audio not working for composite

--- httpserver/app/main/UserSession.java
+++ httpserver/app/main/UserSession.java
@@ -91,7 +91,7 @@ public class UserSession implements Closeable, Comparable<UserSession> {
 		endPoint.connect(compositePort, MediaType.AUDIO); 
 		compositePort.connect(mixerPoint, MediaType.VIDEO);
 		compositePort.connect(mixerPoint, MediaType.AUDIO);
-		endPoint.connect(endPoint);
+		endPoint.connect(endPoint, MediaType.VIDEO);
 
 
 		recorder = new MyRecorder(endPoint, room, new MyRecorder.RecorderHandler() {
