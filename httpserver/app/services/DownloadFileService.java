e9bccd8e4b3524cea1c667f3fa74b8f44766ce1c
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-09-13 01:45:51 +0100

    [ADD] simple chat

--- httpserver/app/services/DownloadFileService.java
+++ httpserver/app/services/DownloadFileService.java
@@ -8,48 +8,41 @@ import com.mongodb.gridfs.GridFSDBFile;
 
 public class DownloadFileService extends Service<byte[]> {
 
-	
 	private String fileName;
+
 	public DownloadFileService(String fileName) {
 		// TODO Auto-generated constructor stub
 		this.fileName = fileName;
 	}
-	
-	
+
 	@Override
 	public byte[] dispatch() {
-		
-		
 
 		try {
 			GridFSDBFile file = files.findOne(fileName);
-			InputStream is = file.getInputStream();
-			
-			// Process the objectData stream.
-			
-			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			if (file != null) {
+				InputStream is = file.getInputStream();
 
-			int nRead;
-			byte[] data = new byte[16384];
+				// Process the objectData stream.
 
-			while ((nRead = is.read(data, 0, data.length)) != -1) {
-			  buffer.write(data, 0, nRead);
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+				int nRead;
+				byte[] data = new byte[16384];
+
+				while ((nRead = is.read(data, 0, data.length)) != -1) {
+					buffer.write(data, 0, nRead);
+				}
+
+				buffer.flush();
+
+				is.close();
+				return buffer.toByteArray();
 			}
-
-			buffer.flush();
-			
-			
-			
-			is.close();
-			return buffer.toByteArray();
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		
-		
 
 		return null;
 	}
