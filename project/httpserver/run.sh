95f9ef2d007920df3837f8a59102d3967459f582
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-11-27 02:54:29 +0000

    [ADD] docker ready run.sh

--- project/httpserver/run.sh
+++ project/httpserver/run.sh
@@ -1,3 +1,5 @@
-sudo service kurento-media-server-6.0 stop
+sudo start mongod
+sudo service kurento-repo start
 sudo service kurento-media-server-6.0 start
+
 nohup ./activator clean ~run -Dhttp.port=2080 -Dhttps.port=2443 &
