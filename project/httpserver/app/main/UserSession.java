cd4f7c66dc3c4067c952ea97c0a16229f8901be2
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-11-30 02:42:23 +0000

    [ADD] beggining group record

--- project/httpserver/app/main/UserSession.java
+++ project/httpserver/app/main/UserSession.java
@@ -263,6 +263,7 @@ public class UserSession implements Closeable, Comparable<UserSession> {
 		String owner = session!=null? session.getUser().getId().toString() : room.getGroupId();
 		
 		try {
+			System.out.println("GET REC for owner "+ owner + " | isgroup="+session == null);
 			GetCurrentRecordingService service = new GetCurrentRecordingService(user.getId().toString(),
 					room.getGroupId(), owner, currentTime);
 			Recording rec = service.execute();
