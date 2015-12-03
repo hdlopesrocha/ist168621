c545546e208945ff0edeb27b78b41b826c4f78cb
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-12-03 03:17:10 +0000

    [FIX] audio levels

--- project/httpserver/app/main/MyRecorder.java
+++ project/httpserver/app/main/MyRecorder.java
@@ -46,7 +46,7 @@ public class MyRecorder {
 						recorder.record();
 
 						try {
-							Thread.sleep(1000);
+							Thread.sleep(2000);
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
