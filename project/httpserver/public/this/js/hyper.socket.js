188dda2936186ce4ca2ae72a9694c88eea1fcb8f
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-12-03 02:40:33 +0000

    [FIX] audio levels

--- project/httpserver/public/this/js/hyper.socket.js
+++ project/httpserver/public/this/js/hyper.socket.js
@@ -248,7 +248,6 @@ var Kurento = new (function() {
 
 				// XXX [CLIENT_OFFER_01] XXX
 				navigator.getUserMedia(local_constraints, function(stream) {
-					audioFunction(stream);
 					Kurento.peerConnection["main"].addStream(stream);
 					Kurento.peerConnection["main"].createOffer(function (lsd) {		
 						console.log("createOfferToSendReceive",lsd);
@@ -260,6 +259,8 @@ var Kurento = new (function() {
 								name : "main",
 								data : lsd
 							}));
+							audioFunction(stream);
+
 						}, logError);
 					}, logError,remote_constraints);
 				}, logError);
