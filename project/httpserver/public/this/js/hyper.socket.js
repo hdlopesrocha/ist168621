154fc719ec30bbee547d82d787ebdda6814645e5
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-12-03 01:11:41 +0000

    [FIX] audio levels

--- project/httpserver/public/this/js/hyper.socket.js
+++ project/httpserver/public/this/js/hyper.socket.js
@@ -57,7 +57,7 @@ function audioFunction(stream){
 			var currentAudioLevel = average(inputLevels);
 			maxAudioLevel = Math.max(maxAudioLevel, currentAudioLevel);
 			var perc = currentAudioLevel/maxAudioLevel;
-			$("#soundIcon").attr("class",perc > 0.1 ?"fa fa-microphone text-success fa-3" : "fa fa-microphone text-off fa-3");
+			$("#soundIcon").attr("class",perc > 0.05 ?"fa fa-microphone text-success" : "fa fa-microphone text-off");
 			$("#soundValues").html(currentAudioLevel+" / "+maxAudioLevel);
 
 		};
