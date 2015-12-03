0007da58cd6dee390128501dd7b16dd52233a9bd
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-12-03 01:35:31 +0000

    [FIX] audio levels

--- project/httpserver/public/this/js/hyper.socket.js
+++ project/httpserver/public/this/js/hyper.socket.js
@@ -62,7 +62,7 @@ function audioFunction(stream){
 				$("#soundValues").html(currentAudioLevel+" / "+maxAudioLevel);
 			}
 			soundProc=soundProc+1;
-			if(soundProc>32){
+			if(soundProc>16){
 				soundProc = 0;
 			}
 		};
