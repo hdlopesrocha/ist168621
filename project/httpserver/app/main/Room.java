c441c998e75688d3eb08e43797f885757df629ce
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-12-04 00:16:51 +0000

    [ADD] first banner

--- project/httpserver/app/main/Room.java
+++ project/httpserver/app/main/Room.java
@@ -1,17 +1,4 @@
-/*
- * (C) Copyright 2014 Kurento (http://kurento.org/)
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- */
+
 package main;
 
 import java.io.Closeable;
@@ -54,7 +41,6 @@ public class Room implements Closeable {
 
 	public Room(final MediaPipeline mediaPipeline) {
 		this.mediaPipeline = mediaPipeline;
-		
 		this.mediaPipeline.addErrorListener(new EventListener<ErrorEvent>() {
 
 			@Override
